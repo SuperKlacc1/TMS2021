37799ea0b14b24b13636d437c4167740fcba2f37
SuperKlacc1 <SuperKlaccTMS@gmail.com>
2021-03-29 19:49:08 +0300

    Задание два

--- src/task1.java
+++ src/task1.java
@@ -3,11 +3,11 @@ public class task1 {
         int count = 0;
         for (String arg : args) {
             int i = Integer.parseInt(arg);
-            if (i > 0){
-            count++;
+            if (i > 0) {
+                count++;
             }
         }
-        System.out.println("Количество положительных чисел"+count);
+        System.out.println("Количество положительных чисел =" + count);
     }
 
 }
