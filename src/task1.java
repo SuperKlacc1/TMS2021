875f4c2cc113af7be42009290c9c7ffb5f52ec24
SuperKlacc1 <SuperKlaccTMS@gmail.com>
2021-03-29 17:52:26 +0300

    Задание 1

---
+++ src/task1.java
@@ -0,0 +1,13 @@
+public class task1 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (String arg : args) {
+            int i = Integer.parseInt(arg);
+            if (i > 0){
+            count++;
+            }
+        }
+        System.out.println("Количество положительных чисел"+count);
+    }
+
+}
